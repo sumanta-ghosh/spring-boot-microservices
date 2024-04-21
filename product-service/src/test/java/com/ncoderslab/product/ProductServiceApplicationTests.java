f70c3d621149c3535d0369fbabd8475c6569c49f
Sumanta <sumanta.ghosh.mit@gmail.com>
2024-04-22 03:07:59 +0530

    Product-service :: Implement integration testing

--- product-service/src/test/java/com/ncoderslab/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/ncoderslab/product/ProductServiceApplicationTests.java
@@ -1,13 +1,54 @@
 package com.ncoderslab.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                  "name": "SM7dB",
+                  "description": "This is a microphone for content creator",
+                  "price": 45000.00
+                }
+                """;
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("SM7dB"))
+                .body("description", Matchers.equalTo("This is a microphone for content creator"))
+                .body("price", Matchers.equalTo(45000.00F));
+
+    }
 
 }

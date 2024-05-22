8fbe9ce5d9b573e05c02673a47c9e5dc33ec92f9
Sumanta <sumanta.ghosh.mit@gmail.com>
2024-05-23 01:49:52 +0530

    MOdify Order service

--- order-service/src/main/java/com/ncoderslab/order/service/OrderService.java
+++ order-service/src/main/java/com/ncoderslab/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ncoderslab.order.service;
 
+import com.ncoderslab.order.client.InventoryClient;
 import com.ncoderslab.order.dto.OrderRequest;
 import com.ncoderslab.order.model.Order;
 import com.ncoderslab.order.repository.OrderRepository;
@@ -14,16 +15,22 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
 
     public Order placeOrder(OrderRequest orderRequest) {
-        Order order = Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .price(orderRequest.price())
-                .skuCode(orderRequest.skuCode())
-                .quantity(orderRequest.quantity())
-                .build();
-        log.info("Order placed with order number {}", order.getOrderNumber());
-        return orderRepository.save(order);
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
+            Order order = Order.builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .price(orderRequest.price())
+                    .skuCode(orderRequest.skuCode())
+                    .quantity(orderRequest.quantity())
+                    .build();
+            log.info("Order placed with order number {}", order.getOrderNumber());
+            return orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Quantity " + orderRequest.quantity() + " of Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
--- order-service/src/test/java/com/ncoderslab/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/ncoderslab/order/OrderServiceApplicationTests.java
@@ -1,13 +1,59 @@
 package com.ncoderslab.order;
 
+import com.ncoderslab.order.model.Order;
+import com.ncoderslab.order.stub.InventoryStubs;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.testcontainers.containers.MySQLContainer;
 
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+    @LocalServerPort
+    private Integer port;
 
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mySQLContainer.start();
+    }
+
+    @Test
+    void shouldSubmitOrder() {
+        String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1
+                }
+                """;
+
+        InventoryStubs.stubInventoryCall("iphone_15", 1);
+        var orderModel = RestAssured.given()
+                .contentType("application/json")
+                .body(submitOrderJson)
+                .when()
+                .post("/api/order")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .body().as(Order.class);
+        assertThat(orderModel.getSkuCode(), Matchers.is("iphone_15"));
+    }
 }

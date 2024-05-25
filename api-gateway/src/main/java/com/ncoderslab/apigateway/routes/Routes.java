485482dd0c1a408976b233bcbfcaeda15d97ccdb
Sumanta <sumanta.ghosh.mit@gmail.com>
2024-05-26 01:30:08 +0530

    Implement Keycloak

---
+++ api-gateway/src/main/java/com/ncoderslab/apigateway/routes/Routes.java
@@ -0,0 +1,44 @@
+package com.ncoderslab.apigateway.routes;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Value("${product.service}")
+    private String productServiceUrl;
+
+    @Value("${order.service}")
+    private String orderServiceUrl;
+
+    @Value("${inventory.service}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return GatewayRouterFunctions.route("product_service")
+                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http(productServiceUrl))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("order_service")
+                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http(orderServiceUrl))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return GatewayRouterFunctions.route("inventory_service")
+                .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http(inventoryServiceUrl))
+                .build();
+    }
+}
